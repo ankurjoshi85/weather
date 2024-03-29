539e1c5a897336ada542c1e27beb859a9b4f8e56
Ankur <ankurjoshi456@gmail.com>
2024-03-29 22:52:58 +0530

    Setup:[Initial commit]

---
+++ weather/src/test/java/com/ankur/weather/WeatherForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankur.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
